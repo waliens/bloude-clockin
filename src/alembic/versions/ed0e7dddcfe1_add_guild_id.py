c6542b284c2e8be25e8c4fb88c378b63a2033960
waliens <r.mormont@uliege.be>
2022-07-25 13:16:54 +0200

    add guild id

---
+++ src/alembic/versions/ed0e7dddcfe1_add_guild_id.py
@@ -0,0 +1,24 @@
+"""add guild id
+
+Revision ID: ed0e7dddcfe1
+Revises: 
+Create Date: 2022-07-25 11:16:28.012949
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ed0e7dddcfe1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 class Character(Base):
   __tablename__ = "character"
   id = Column(Integer, primary_key=True)
+  id_guild = Column(String(22), primary_key=True)
   id_user = Column(String(22))
   name = Column(String(255))
   is_main = Column(Boolean)
@@ -30,6 +31,7 @@ class Raid(Base):
 
 class Attendance(Base):
   __tablename__ = "attendance"
+  id_guild = Column(String(22), primary_key=True)
   id_character = Column(Integer, ForeignKey('character.id', ondelete="CASCADE"), primary_key=True)
   id_raid = Column(Integer, ForeignKey('raid.id', ondelete="CASCADE"), primary_key=True)
   created_at = Column(DateTime)
@@ -50,6 +52,7 @@ class Item(Base):
 
 class Loot(Base):
   __tablename__ = "loot"
+  id_guild = Column(String(22), primary_key=True)
   id_character = Column(Integer, ForeignKey("character.id", ondelete="CASCADE"), primary_key=True)
   id_item = Column(Integer, ForeignKey("item.id", ondelete="CASCADE"), primary_key=True)
   created_at = Column(DateTime)
