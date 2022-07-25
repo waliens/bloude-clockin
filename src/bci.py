39a8faf8137ebd8f67dbaa64bf01958861d72919
waliens <r.mormont@uliege.be>
2022-07-25 14:58:43 +0200

    load db on connect rather than on ready + dispose of db connect on bot disconnect

--- src/bci.py
+++ src/bci.py
@@ -6,7 +6,11 @@ class BloudeClockInBot(commands.Bot):
   def __init__(self, *args, **kwargs) -> None:
     super().__init__(*args, **kwargs)
 
-    cog_exts = ["hello"] 
+    cog_exts = [
+      "hello", 
+      "character"
+    ]
+
     for cog_ext in cog_exts:
       self.load_extension("cogs.{}".format(cog_ext))
 
@@ -22,5 +26,20 @@ class BloudeClockInBot(commands.Bot):
     return self._db_engine
   
   async def on_ready(self):
+    logging.getLogger().info("Bot `{}` is ready.".format(self.bot_classname))
+    
+  async def on_connect(self):
     self._db_session, self._db_engine = await init_db()
-    logging.getLogger().info("Bot `{}` is running.".format(self.__class__))
+    logging.getLogger().info("Bot `{}` successfully connected to the database.".format(self.bot_classname))
+    logging.getLogger().info("Bot `{}` is connected.".format(self.bot_classname))
+
+  async def on_disconnect(self):
+    self._db_engine.dispose()
+    self._db_engine = None
+    self._db_session = None
+    logging.getLogger().info("Bot `{}` disconnected from the database.".format(self.bot_classname))
+    logging.getLogger().info("Bot `{}` is disconnected.".format(self.bot_classname))
+
+  @property
+  def bot_classname(self):
+    return self.__class__.__name__
