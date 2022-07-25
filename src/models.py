30140b9bc591c9d64734eac6653acf3d2db17b3a
waliens <r.mormont@uliege.be>
2022-07-25 13:25:47 +0200

    fix guild ids

--- src/models.py
+++ src/models.py
@@ -9,14 +9,14 @@ Base = declarative_base()
 class Character(Base):
   __tablename__ = "character"
   id = Column(Integer, primary_key=True)
-  id_guild = Column(String(22), primary_key=True)
+  id_guild = Column(String(22))
   id_user = Column(String(22))
   name = Column(String(255))
   is_main = Column(Boolean)
   created_at = Column(DateTime)
 
   __table_args__ = (
-    UniqueConstraint('id_user', 'name', name='id_user_name_unique_constraint'),
+    UniqueConstraint('id_user', 'name', 'id_guild', name='id_guild_user_name_unique_constraint'),
   )
 
 
@@ -31,7 +31,6 @@ class Raid(Base):
 
 class Attendance(Base):
   __tablename__ = "attendance"
-  id_guild = Column(String(22), primary_key=True)
   id_character = Column(Integer, ForeignKey('character.id', ondelete="CASCADE"), primary_key=True)
   id_raid = Column(Integer, ForeignKey('raid.id', ondelete="CASCADE"), primary_key=True)
   created_at = Column(DateTime)
@@ -52,7 +51,6 @@ class Item(Base):
 
 class Loot(Base):
   __tablename__ = "loot"
-  id_guild = Column(String(22), primary_key=True)
   id_character = Column(Integer, ForeignKey("character.id", ondelete="CASCADE"), primary_key=True)
   id_item = Column(Integer, ForeignKey("item.id", ondelete="CASCADE"), primary_key=True)
   created_at = Column(DateTime)
