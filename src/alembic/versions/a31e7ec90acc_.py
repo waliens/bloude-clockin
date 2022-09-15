405987f08c1c5f4211826fde0a60b07b6df20aca
waliens <r.mormont@uliege.be>
2022-09-15 16:13:58 +0200

    merge heads

---
+++ src/alembic/versions/a31e7ec90acc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a31e7ec90acc
+Revises: 9bb8af50400e, c357bcb06172
+Create Date: 2022-09-15 14:13:02.741760
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a31e7ec90acc'
+down_revision = ('9bb8af50400e', 'c357bcb06172')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
